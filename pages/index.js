487ca48f27319ff50bf9290aa076e8b6bf632c7a
Caio Alexandre <caioalexandre.br@gmail.com>
2021-12-07 18:08:48 -0300

    feat: current game request in progress

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import styled from 'styled-components'
 
@@ -9,6 +9,7 @@ const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
 `;
 
 const Input = styled.input`
@@ -23,28 +24,93 @@ const Button = styled.button`
   color: white;
 `;
 
-export default function Home() {
-  const [valueInput, setValueInput] = useState('sushi lover')
+const List = styled.ul`
+  margin-top: 40px;
+  background: white;
+  padding: 20px;
+`;
 
-  const searchSummonerName = (userName) => {
+const useSummonerId = () => {
+  const [summonerId, setSummonerId] = useState(null);
+
+  const getSummonerId = (userName) => {
     var RiotRequest = require('riot-lol-api');
 
     var riotRequest = new RiotRequest(process.env.NEXT_PUBLIC_API_KEY);
 
-    riotRequest.request('br1', 'summoner', `/lol/summoner/v4/summoners/by-name/${userName}`, function(err, data) {
-      err && console.log('error: ', err)
-      data && console.log('data: ', data)
-    });
-  }
+    riotRequest.request('br1', 'summoner', `/lol/summoner/v4/summoners/by-name/${userName}`, function(error, data) {
+      if (error) {
+        console.log(`Usuário ${userName} não encontrado.`);
+      }
 
-  const onSubmit = () => {
-    searchSummonerName(valueInput);
-  }
+      if (data.id) {
+        setSummonerId(data.id);
+      }
+    });
+  };
+
+  return [summonerId, getSummonerId];
+};
+
+const useCurrentGameInfo = () => {
+  const [currentGameInfo, setCurrentGameInfo] = useState(null);
+
+  const getCurrentGameInfo = (encryptedSummonerId) => {
+     var RiotRequest = require('riot-lol-api');
+
+    var riotRequest = new RiotRequest(process.env.NEXT_PUBLIC_API_KEY);
+
+    riotRequest.request('br1', 'summoner', `/lol/spectator/v4/active-games/by-summoner/${encryptedSummonerId}`, function(error, data) {
+      if (error) {
+        console.log(`Usuário não está jogando neste momento.`);
+      }
+
+      if (data.gameId) {
+        setCurrentGameInfo(data);
+      }
+    });
+  };
+
+  return [currentGameInfo, getCurrentGameInfo];
+};
+
+export default function Home() {
+  const [valueInput, setValueInput] = useState('');
+
+  const [summonerId, getSummonerId] = useSummonerId();
+  const [currentGameInfo, getCurrentGameInfo] = useCurrentGameInfo();
+
+  const [enemyTeam, setEnemyTeam] = useState([]);
+
+  useEffect(() => {
+    if (summonerId) {
+      getCurrentGameInfo(summonerId);
+    }
+  }, [summonerId]);
+
+  useEffect(() => {
+    setEnemyTeam(currentGameInfo?.participants.filter(user => (user.teamId === 100)))
+  }, [currentGameInfo]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getSummonerId(valueInput);
+  };
 
   return (
     <Content>
-      <Input value={valueInput} onChange={e => setValueInput(e.target.value)}/>
-      <Button onClick={() => onSubmit()}>Search</Button>
+      <form onSubmit={onSubmit}>
+        <Input value={valueInput} onChange={e => setValueInput(e.target.value)}/>
+        <Button type="submit">Search</Button>
+      </form>
+      {enemyTeam?.length && (
+          <List>
+            {enemyTeam.map((enemy, i) => (
+              <li key={i}>{enemy.summonerName}</li>
+            ))}
+          </List>
+        )
+      }
     </Content>
   )
 }
